0f467ab69af55c3732ed82c1af18fa16164de4d3
Barbara Barabas <bbarbara092@gmail.com>
2021-03-03 12:48:49 +0100

    Got pokemonId from the url

--- src/Components/Pokemon.js
+++ src/Components/Pokemon.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-const Pokemon = () => {
+const Pokemon = props => {
+    const { match } = props;
+    const { params } = match;
+    const { pokemonId } = params;
+
     return (
         <div>
-            This is the pokemon page.
+            {`This is the pokemon page. ${pokemonId}`}
         </div>
     )
 }
