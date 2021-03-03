685f4917c97ccc22fa76f86da6bd46ba8c80830c
Barbara Barabas <bbarbara092@gmail.com>
2021-03-03 15:19:14 +0100

    Used axios to fetch and display data in the pokemonlist page

--- src/Components/Pokedex.js
+++ src/Components/Pokedex.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import { AppBar, Toolbar, Grid, Card, CardContent, CircularProgress, CardMedia, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import MokedData from '../MokedData';
+import axios from "axios";
 
 const useStyles = makeStyles({
     pokedexContainer: {
@@ -22,12 +22,29 @@ const useStyles = makeStyles({
 const Pokedex = props => {
     const {history} = props;
     const classes = useStyles;
-    const [pokemonData, setPokemonData] = useState(MokedData);
+    const [pokemonData, setPokemonData] = useState({});
+
+    useEffect(() => {
+        axios
+            .get(`https://pokeapi.co/api/v2/pokemon`)
+            .then((response) => {
+                const { data } = response;
+                const { results} = data;
+                const newPokemonData = {};
+                results.forEach((pokemon, index) => {
+                    newPokemonData[index + 1] = {
+                        id: index + 1,
+                        name: pokemon.name,
+                        sprite: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`
+                    }
+                });
+                setPokemonData(newPokemonData);
+            });
+    
+    }, []);
 
     const getPokemonCard = (pokemonId) => {
-        console.log(pokemonData[`${pokemonId}`]);
-        const {id, name} = pokemonData[`${pokemonId}`];
-        const sprite = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
+        const {id, name, sprite} = pokemonData[pokemonId];
 
         return (
             <Grid item xs={12} sm={4} key={pokemonId}>
