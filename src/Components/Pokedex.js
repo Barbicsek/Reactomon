15f1f4ae6afe99d682921d522914618ed0369846
Barbara Barabas <bbarbara092@gmail.com>
2021-03-03 14:08:46 +0100

    Display pokemon's image on the pokemon list page

--- src/Components/Pokedex.js
+++ src/Components/Pokedex.js
@@ -1,39 +1,55 @@
-import React from 'react';
-import { AppBar, Toolbar, Grid, Card, CardContent } from "@material-ui/core";
+import React, {useState, useEffect} from 'react';
+import { AppBar, Toolbar, Grid, Card, CardContent, CircularProgress, CardMedia } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import MokedData from '../MokedData';
 
 const useStyles = makeStyles({
     pokedexContainer: {
         paddingTop: "20px",
         paddingLeft: "50px",
         paddingRight: "50px",
+    },
+    cardMedia: {
+        margin: "auto",
     }
 });
 
-const getPokemonCard = () => {
-    return (
-        <Grid item xs={12} sm={4}>
-            <Card>
-                <CardContent>HI</CardContent>
-            </Card>
-        </Grid>
-    )
-
-};
-
 const Pokedex = () => {
     const classes = useStyles;
+    const [pokemonData, setPokemonData] = useState(MokedData);
+
+    const getPokemonCard = (pokemonId) => {
+        console.log(pokemonData[`${pokemonId}`]);
+        const {id, name} = pokemonData[`${pokemonId}`];
+        const sprite = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
+
+        return (
+            <Grid item xs={12} sm={4} key={pokemonId}>
+                <Card>
+                    <CardMedia className={classes.cardMedia}
+                    image={sprite}
+                    style={{width: "130px", height: "130px"}}>
+                        
+                    </CardMedia>
+                    <CardContent>HI</CardContent>
+                </Card>
+            </Grid>
+        )
+    };
+
     return (
         <>
             <AppBar position="static">
                 <Toolbar/>
             </AppBar>
-            <Grid container spacing={2} className={classes.pokedexContainer}>
-                {getPokemonCard()}
-                {getPokemonCard()}
-                {getPokemonCard()}
-                {getPokemonCard()}
+            {pokemonData ? (
+                <Grid container spacing={2} className={classes.pokedexContainer}>
+                    {Object.keys(pokemonData).map(pokemonId => 
+                        getPokemonCard(pokemonId))}
             </Grid>
+            ) : (
+                <CircularProgress/>
+            )}
         </>
     )
 }
