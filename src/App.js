2f79d24b5c361f0a12ecd14726a35359caf92556
Barbara Barabas <bbarbara092@gmail.com>
2021-03-03 18:44:44 +0100

    Added new route to the tpylist component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import {Route, Switch} from 'react-router-dom';
 import PokemonList from './Components/PokemonList';
 import PokemonDetails from './Components/PokemonDetails';
+import TypeList from './Components/TypeList';
+
 
 
 const App = props => {
@@ -9,11 +11,13 @@ const App = props => {
   return (
     <Switch>
       <Route exact path='/' render={(props) => <PokemonList {...props}/>}></Route>
+      <Route exact path='/typelist' render={(props) => <TypeList {...props}/>}></Route>
       <Route
         exact
         path='/:pokemonId'
         render={(props) => <PokemonDetails {...props}/>}>
       </Route>
+     
     </Switch>
 
   )
