e26459eacd8114dc70f0ad2aff0749b0cbf0468f
Barbara Barabas <bbarbara092@gmail.com>
2021-03-03 12:38:34 +0100

    Routing is working properly

--- src/App.js
+++ src/App.js
@@ -1,42 +1,23 @@
-import Header from './components/Header'
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-import PokemonList from './components/PokemonList'
-import PokemonDetail from './components/PokemonDetail'
-import TypeList from './components/TypeList'
-import axios from "axios"
-import React, {useState, useEffect} from "react"
+import {Route, Switch} from 'react-router-dom';
+import axios from "axios";
+import React, {useState, useEffect} from "react";
+import Pokedex from './Components/Pokedex';
+import Pokemon from './Components/Pokemon';
 
 
 const App = props => {
 
-  const [pokemons, setPokemons] = useState([]);
-
-  const [selectedCharacter, setSelectedCharacter] = useState(1);
-
-  const charSelectHandler = event => {
-    const charId = event.target.value;
-    setSelectedCharacter(charId);
-  };
-
-  useEffect(() => {
-      axios.get(`https://pokeapi.co/api/v2/pokemon`).then(response => {
-          const pokemons = response.data.results;
-          setPokemons(pokemons);
-      });
-  }, [] );
-
 
   return (
-    <Router>
-      <div className='container'>
-        <Switch>
-          <Route path='/' exact component={Header}/>
-          <Route path='/pokemons' render={props => ( <PokemonList pokemons={pokemons} selectedChar={selectedCharacter} onCharSelect={charSelectHandler}/>)}/>
-          <Route path='/types' component={TypeList}/>
-          <PokemonDetail selectedChar={selectedCharacter} />
-        </Switch>
-      </div>
-    </Router>
+    <Switch>
+      <Route exact path='/' render={(props) => <Pokedex {...props}/>}></Route>
+      <Route
+        exact
+        path='/:pokemonId'
+        render={(props) => <Pokemon {...props}/>}>
+      </Route>
+    </Switch>
+
   )
 }
 
---
+++ src/Components/Pokemon.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Pokemon = () => {
+    return (
+        <div>
+            This is the pokemon page.
+        </div>
+    )
+}
+
+export default Pokemon;
+
