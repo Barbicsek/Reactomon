45d13f61fe1e4d404b151d16c41752575db8ff0e
Barbara Barabas <bbarbara092@gmail.com>
2021-03-03 13:26:08 +0100

    Installed material userinterface framework and added some style to the pokedex page

--- src/Components/Pokedex.js
+++ src/Components/Pokedex.js
@@ -1,11 +1,41 @@
-import React from 'react'
+import React from 'react';
+import { AppBar, Toolbar, Grid, Card, CardContent } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+    pokedexContainer: {
+        paddingTop: "20px",
+        paddingLeft: "50px",
+        paddingRight: "50px",
+    }
+});
+
+const getPokemonCard = () => {
+    return (
+        <Grid item xs={12} sm={4}>
+            <Card>
+                <CardContent>HI</CardContent>
+            </Card>
+        </Grid>
+    )
+
+};
 
 const Pokedex = () => {
+    const classes = useStyles;
     return (
-        <div>
-            This is the pokedex page.
-        </div>
+        <>
+            <AppBar position="static">
+                <Toolbar/>
+            </AppBar>
+            <Grid container spacing={2} className={classes.pokedexContainer}>
+                {getPokemonCard()}
+                {getPokemonCard()}
+                {getPokemonCard()}
+                {getPokemonCard()}
+            </Grid>
+        </>
     )
 }
 
-export default Pokedex
+export default Pokedex;
