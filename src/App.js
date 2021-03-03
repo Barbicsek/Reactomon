5bfb591df1eb9aec88b709b39a674e3d402818f8
Barbara Barabas <bbarbara092@gmail.com>
2021-03-03 11:59:06 +0100

    Started again the project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import Header from './components/Header'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import PokemonList from './components/PokemonList'
+import PokemonDetail from './components/PokemonDetail'
 import TypeList from './components/TypeList'
 import axios from "axios"
 import React, {useState, useEffect} from "react"
 
 
-const App = () => {
+const App = props => {
 
   const [pokemons, setPokemons] = useState([]);
 
-  const pokemonId = () => {
-    const min = Math.ceil(1)
-    const max = Math.floor(150)
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
+  const [selectedCharacter, setSelectedCharacter] = useState(1);
+
+  const charSelectHandler = event => {
+    const charId = event.target.value;
+    setSelectedCharacter(charId);
+  };
 
   useEffect(() => {
-      axios.get(`https://pokeapi.co/api/v2/pokemon/`).then(response => {
+      axios.get(`https://pokeapi.co/api/v2/pokemon`).then(response => {
           const pokemons = response.data.results;
           setPokemons(pokemons);
       });
@@ -29,8 +31,9 @@ const App = () => {
       <div className='container'>
         <Switch>
           <Route path='/' exact component={Header}/>
-          <Route path='/pokemons' render={props => ( <PokemonList pokemons={pokemons}/>)}/>
+          <Route path='/pokemons' render={props => ( <PokemonList pokemons={pokemons} selectedChar={selectedCharacter} onCharSelect={charSelectHandler}/>)}/>
           <Route path='/types' component={TypeList}/>
+          <PokemonDetail selectedChar={selectedCharacter} />
         </Switch>
       </div>
     </Router>
