6dd379e6acee4982a7053684e6de7fd01fb92838
Barbara Barabas <bbarbara092@gmail.com>
2021-03-04 17:23:20 +0100

    Added catched pokemon provider component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import PokemonList from './Components/PokemonList';
 import PokemonDetails from './Components/PokemonDetails';
 import TypeList from './Components/TypeList';
 import TypeDetails from './Components/TypeDetails';
+import CatchedPokemonsList from './Components/CatchedPokemonsList';
 import {PokemonProvider} from './Contexts/PokemonContext';
 import {TypeProvider} from './Contexts/TypeContext';
+import {CatchedPokeProvider} from './Contexts/CatchedPokemonContext';
 import Home from "./Components/Home";
-import { Navbar, Nav } from "react-bootstrap";
-
 
 
 
@@ -17,21 +17,24 @@ const App = props => {
   return (
     <PokemonProvider>
       <TypeProvider>
-        <Switch>
-          <Route exact path='/' component={Home} ></Route>
-          <Route exact path='/pokemons' render={(props) => <PokemonList {...props}/>}></Route>
-          <Route exact path='/typelist' render={(props) => <TypeList {...props}/>}></Route>
-          <Route
-            exact
-            path='/pokemons/:pokemonId'
-            render={(props) => <PokemonDetails {...props}/>}>
-          </Route>
-          <Route
-            exact
-            path='/typelist/:typeId'
-            render={(props) => <TypeDetails {...props}/>}>
-          </Route>
-        </Switch>
+        <CatchedPokeProvider>
+          <Switch>
+            <Route exact path='/' component={Home} ></Route>
+            <Route exact path='/pokemons' render={(props) => <PokemonList {...props}/>}></Route>
+            <Route exact path='/catchedpokemons' render={(props) => <CatchedPokemonsList {...props}/>}></Route>
+            <Route exact path='/typelist' render={(props) => <TypeList {...props}/>}></Route>
+            <Route
+              exact
+              path='/pokemons/:pokemonId'
+              render={(props) => <PokemonDetails {...props}/>}>
+            </Route>
+            <Route
+              exact
+              path='/typelist/:typeId'
+              render={(props) => <TypeDetails {...props}/>}>
+            </Route>
+          </Switch>
+        </CatchedPokeProvider>
       </TypeProvider>
     </PokemonProvider>
 
