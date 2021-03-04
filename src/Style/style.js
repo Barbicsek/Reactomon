ebfcc55b0ce8ad95adcedaac41b90fee0be346ab
Barbara Barabas <bbarbara092@gmail.com>
2021-03-04 21:41:22 +0100

    Installed node-sass and renamed the css file

---
+++ src/Style/style.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+
+
+export const useStyles = makeStyles({
+    pokedexContainer: {
+        paddingTop: "20px",
+        paddingLeft: "50px",
+        paddingRight: "50px",
+    },
+    cardMedia: {
+        margin: "auto",
+    },
+
+    cardContent: {
+        textAlign: "center",
+    },
+    titleText: {
+        textAlign: "center",
+        color: "red",
+    }
+});
+
