129042d4ce9793a369007fca6ca397304afd4e72
Barbara Barabas <bbarbara092@gmail.com>
2021-03-04 21:36:29 +0100

    Implemented themeContext which can store the drakmode state

---
+++ src/Contexts/ThemeContext.js
@@ -0,0 +1,17 @@
+import React, {createContext, useState } from 'react';
+
+export const ThemeContext = createContext();
+
+export const ThemeContextProvider = (props) => {
+    const [lightTheme, setLightTheme] = useState(true);
+
+    const toggleTheme = () => {
+        setLightTheme(!lightTheme);
+    }
+    const { children } = props;
+    return (
+        <ThemeContext.Provider value={{ lightTheme, toggleTheme }}>
+             {children}
+        </ThemeContext.Provider>
+    )
+}
